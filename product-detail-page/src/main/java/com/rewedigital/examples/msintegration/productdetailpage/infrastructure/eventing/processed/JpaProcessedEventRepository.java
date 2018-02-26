f69c93154ff5da0de304f90f072c3499326f0960
Andreas Kluth <andreas.kluth@rewe-digital.com>
2018-02-26 15:12:59 +0100

    Removed projects used for demos.

--- product-detail-page/src/main/java/com/rewedigital/examples/msintegration/productdetailpage/infrastructure/eventing/processed/JpaProcessedEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.processed;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaProcessedEventRepository extends JpaRepository<ProcessedEventEntity, String> {
-
-    ProcessedEventEntity findByTopicAndKey(String topic, String key);
-
-}
--- product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/product/JpaProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rewedigital.examples.msintegration.productinformation.product;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaProductRepository extends JpaRepository<Product, String> {
-
-}
